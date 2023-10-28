c7b0453e1bfb24bbe1094c53f98eaa77e8bca8d5
Muhammad Daffa'I Rafi Prasetyo <daffarafip24@gmail.com>
2023-10-28 22:37:05 +0700

    shoppincart bookshelf models fix

--- shop/models.py
+++ shop/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.core.validators import MinValueValidator
 from books.models import Book
-
+from readme.models import User
 # Create your models here.
 
 
@@ -10,3 +10,29 @@ class ShopItem(models.Model):
     price = models.IntegerField()
     book = models.OneToOneField(
         Book, on_delete=models.CASCADE, related_name='shop_item')
+
+
+class Bookshelf(models.Model):
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, related_name='bookshelf')
+
+
+class BookshelfItem(models.Model):
+    amount = models.IntegerField(validators=[MinValueValidator(0)], default=1)
+    item = models.ForeignKey(
+        ShopItem, on_delete=models.SET_NULL, null=True, related_name='bookshelves')
+    bookshelf = models.ForeignKey(
+        Bookshelf, on_delete=models.CASCADE, related_name='books')
+
+
+class ShoppingCart(models.Model):
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, related_name='shopping_cart')
+
+
+class ShoppingCartItem(models.Model):
+    amount = models.IntegerField(validators=[MinValueValidator(0)], default=1)
+    item = models.ForeignKey(
+        ShopItem, on_delete=models.CASCADE, related_name='shopping_carts')
+    shopping_cart = models.ForeignKey(
+        ShoppingCart, on_delete=models.CASCADE, related_name='books')
