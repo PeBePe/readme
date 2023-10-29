c7b3586a1fcf64371690ccbec42091ba39770603
Muhammad Daffa'I Rafi Prasetyo <daffarafip24@gmail.com>
2023-10-30 04:18:17 +0700

    fix deployment

--- readme/settings.py
+++ readme/settings.py
@@ -64,6 +64,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     "django_browser_reload.middleware.BrowserReloadMiddleware",
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'readme.urls'
