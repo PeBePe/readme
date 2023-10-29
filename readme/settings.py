2afc77e2a4797b9c7a5fef7b648c3ecd54b28e0d
Muhammad Daffa'I Rafi Prasetyo <daffarafip24@gmail.com>
2023-10-29 23:30:21 +0700

    fix deploymeny

--- readme/settings.py
+++ readme/settings.py
@@ -13,12 +13,14 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 from dotenv import load_dotenv
+import environ  # Tambahkan kode berikut
 
 load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+env = environ.Env()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
