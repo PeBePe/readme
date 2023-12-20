14e41d94459733b0b97361f30f7a3df77223cf91
khalishana <khalisha.hana10@gmail.com>
2023-12-20 08:31:35 +0700

    add cited count and cited users

--- quotes/views.py
+++ quotes/views.py
@@ -1,8 +1,7 @@
-import json
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from quotes.models import Quote, QuoteCited
-from django.http import HttpResponse, HttpResponseRedirect, HttpResponseBadRequest
+from django.http import HttpResponseRedirect, HttpResponseBadRequest
 from django.urls import reverse
 from quotes.forms import ProductForm
 from django.shortcuts import get_object_or_404
@@ -13,8 +12,7 @@ from django.db.models import Q
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_http_methods
 from django.forms.models import model_to_dict
-from django.core.serializers import serialize
-from .serializers import QuoteSerializer
+
 
 # Create your views here.
 
@@ -147,17 +145,6 @@ def api_get_quotes(request):
     # ngambil daftar kutipan dan pengguna yang mengutipnya
     quoted_quotes = QuoteCited.objects.select_related(
         'quote_id', 'user_id').all().values()
-    
-     # Tambahkan username ke setiap quote
-    for quote in quotes:
-        #user_id = quote['user']
-        user_id = quote.get('user', None)
-        try:
-            username = User.objects.get(id=user_id).username
-            quote['username'] = username
-        except User.DoesNotExist:
-            # Handle exception jika user_id tidak ditemukan
-            quote['username'] = None
 
     quotes = [{
         "id": quote.id,
@@ -165,7 +152,14 @@ def api_get_quotes(request):
         "updated_at": quote.updated_at,
         "quote": quote.quote,
         "user_id": quote.user.id,
-        "username": quote.user.username
+        "username": quote.user.username,
+        "cited_count": QuoteCited.objects.filter(quote_id=quote).count(),
+        "cited_users": [ #Daftar akun yang melakukan sitasi
+            {
+                "user_id": qc.user_id.id,
+                "username": qc.user_id.username
+            } for qc in QuoteCited.objects.filter(quote_id=quote).select_related('user_id')
+        ]
     } for quote in quotes]
 
     context = {
@@ -247,6 +241,7 @@ def api_edit_quote(request, quote_id):
     form.save()
     return JsonResponse({"status": True, "message": "Quote berhasil diedit!", "quote": quote_dict}, status=200)
 
+
 @csrf_exempt
 @require_http_methods(["POST"])
 def api_cite_quote(request, quote_id):
@@ -277,7 +272,7 @@ def api_cite_quote(request, quote_id):
             "id": quote.pk,
             'created_at': quote.created_at,
             'updated_at': quote.updated_at,
-            "quote": quote.quote
+            "quote": quote.quote,
         },
         'cited_count': int(cited_count),
         'user_cited': user_cited,
@@ -285,7 +280,7 @@ def api_cite_quote(request, quote_id):
             "id": quote.id,
             'created_at': quote.created_at,
             'updated_at': quote.updated_at,
-            "quote": quote.quote
+            "quote": quote.quote,
         } for quote in quoted_quotes]
     }
 
@@ -299,68 +294,3 @@ def api_cite_quote(request, quote_id):
         return JsonResponse({"status": True, "message": "Berhasil mengutip quote", "quote": context}, status=200)
 
     return JsonResponse({"status": False, "message": "Terjadi kesalahan"}, status=500)
-
-# def show_json(request): #nampilin code dalam bentuk json
-#     data = Quote.objects.all()
-#     return HttpResponse(serialize("json", data), content_type="application/json")
-
-def show_json(request):
-    quotes = Quote.objects.all()
-    quotes_json = json.loads(serialize('json', quotes))
-
-    # Tambahkan username ke setiap quote
-    for quote in quotes_json:
-        user_id = quote['fields']['user']
-        username = User.objects.get(id=user_id).username
-        quote['fields']['username'] = username
-
-    return HttpResponse(json.dumps(quotes_json), content_type="application/json")
-
-def show_json_by_id(request, id):
-    data = Quote.objects.filter(pk=id)
-    return HttpResponse(serialize("json", data), content_type="application/json")
-
-# def show_json(request):
-#     quotes = Quote.objects.all()
-#     serializer = QuoteSerializer(quotes, many=True)
-#     return JsonResponse(serializer.data, safe=False)
-
-# def show_json_by_id(request, id):
-#     try:
-#         quote = Quote.objects.get(pk=id)
-#     except Quote.DoesNotExist:
-#         return JsonResponse({'error': 'Quote not found'}, status=404)
-#     serializer = QuoteSerializer(quote)
-#     return JsonResponse(serializer.data)
-
-# def show_json(request):
-#     # Ambil semua data dari model Quote
-#     quotes_data = Quote.objects.all()
-    
-#     # Ambil semua data dari model QuoteCited
-#     quoted_data = QuoteCited.objects.all()
-    
-#     # Serialisasi data dari kedua model
-#     quotes_json = serialize("json", quotes_data)
-#     quoted_json = serialize("json", quoted_data)
-
-#     # Gabungkan hasil serialisasi
-#     combined_data = quotes_json + quoted_json
-    
-#     return JsonResponse(combined_data, safe=False, content_type="application/json")
-
-# def show_json_by_id(request, id):
-#     # Ambil data dari model Quote berdasarkan ID
-#     quote_data = Quote.objects.filter(pk=id)
-    
-#     # Ambil data dari model QuoteCited berdasarkan ID
-#     quoted_data = QuoteCited.objects.filter(pk=id)
-    
-#     # Serialisasi data dari kedua model
-#     quote_json = serialize("json", quote_data)
-#     quoted_json = serialize("json", quoted_data)
-
-#     # Gabungkan hasil serialisasi
-#     combined_data = quote_json + quoted_json
-    
-#     return JsonResponse(combined_data, safe=False, content_type="application/json")
